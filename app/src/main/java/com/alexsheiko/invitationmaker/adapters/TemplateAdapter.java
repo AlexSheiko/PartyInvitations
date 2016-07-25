effc46e05f8747003e9d33d859ea2ac8cc2de439
AlexeySheiko <alexeysheiko@hotmail.com>
2016-07-25 19:03:41 +0200

    Allow to edit selecting image

--- app/src/main/java/com/alexsheiko/invitationmaker/adapters/TemplateAdapter.java
+++ app/src/main/java/com/alexsheiko/invitationmaker/adapters/TemplateAdapter.java
@@ -3,6 +3,8 @@ package com.alexsheiko.invitationmaker.adapters;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,11 @@ import android.widget.ImageView;
 import com.adobe.creativesdk.aviary.AdobeImageIntent;
 import com.alexsheiko.invitationmaker.R;
 import com.bumptech.glide.Glide;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class TemplateAdapter extends ArrayAdapter<Integer> {
@@ -33,11 +40,16 @@ public class TemplateAdapter extends ArrayAdapter<Integer> {
         int resId = getItem(position);
         Glide.with(getContext()).load(resId).fitCenter().centerCrop().into(((ImageView) view));
         view.setOnClickListener(view1 -> {
-            Uri imageUri = Uri.parse("android.resource://"
-                    + getContext().getPackageName()
-                    + "/drawable/"
-                    + getContext().getResources().getResourceEntryName(resId)
-            );
+            openImageEditor(resId);
+        });
+        return view;
+    }
+
+    private void openImageEditor(int resId) {
+        try {
+            Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), resId);
+            File file = convertBitmapToFile(bitmap);
+            Uri imageUri = Uri.fromFile(file);
 
             Intent imageEditorIntent = new AdobeImageIntent.Builder(getContext())
                     .setData(imageUri)
@@ -45,7 +57,26 @@ public class TemplateAdapter extends ArrayAdapter<Integer> {
 
             /* Start the Image Editor with request code 1 */
             ((Activity) getContext()).startActivityForResult(imageEditorIntent, 1);
-        });
-        return view;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    private File convertBitmapToFile(Bitmap bitmap) throws IOException {
+        // Create a file to write bitmap data
+        File file = new File(getContext().getCacheDir(), "image.png");
+
+        // Convert bitmap to byte array
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0 /*ignored for PNG*/, bos);
+        byte[] bytes = bos.toByteArray();
+
+        // Write the bytes in file
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(bytes);
+        fos.flush();
+        fos.close();
+        return file;
+    }
+
 }
