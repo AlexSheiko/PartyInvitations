fc50ae41b06f0698866cb5bb84dfcdfbdb3005e6
AlexeySheiko <alexeysheiko@hotmail.com>
2016-08-04 16:32:43 +0200

    Restore the previous version of Creative SDK

--- app/src/main/java/com/alexsheiko/invitationmaker/InvitationApplication.java
+++ app/src/main/java/com/alexsheiko/invitationmaker/InvitationApplication.java
@@ -2,14 +2,14 @@ package com.alexsheiko.invitationmaker;
 
 import android.app.Application;
 
+import com.adobe.creativesdk.aviary.IAviaryClientCredentials;
 import com.adobe.creativesdk.foundation.AdobeCSDKFoundation;
-import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
 import com.crashlytics.android.Crashlytics;
 
 import io.fabric.sdk.android.Fabric;
 
 
-public class InvitationApplication extends Application implements IAdobeAuthClientCredentials {
+public class InvitationApplication extends Application implements IAviaryClientCredentials {
 
     private static final String CREATIVE_SDK_CLIENT_ID = "56d4547128e84e079f6e1ef131607153";
     private static final String CREATIVE_SDK_CLIENT_SECRET = "a46a3b39-4a9e-42c2-a5bc-6d7487e21943";
@@ -29,5 +29,10 @@ public class InvitationApplication extends Application implements IAdobeAuthClie
     @Override
     public String getClientSecret() {
         return CREATIVE_SDK_CLIENT_SECRET;
+    }
+
+    @Override
+    public String getBillingKey() {
+        return "";
     }
 }
