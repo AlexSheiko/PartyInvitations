f7e7e93312699894c3c5e2b2ab018f5b9cb75bb6
AlexeySheiko <alexeysheiko@hotmail.com>
2016-11-11 02:56:13 +0100

    Wait for video ad to load

--- app/src/main/java/com/alexsheiko/invitationmaker/AdProviderVideo.java
+++ app/src/main/java/com/alexsheiko/invitationmaker/AdProviderVideo.java
@@ -4,15 +4,18 @@ import android.content.Context;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
 import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
 
-class AdProviderVideo {
+class AdProviderVideo implements RewardedVideoAdListener {
 
     private RewardedVideoAd mAd;
+    private boolean mClickedShow = false;
 
     void prepare(Context context) {
         mAd = MobileAds.getRewardedVideoAdInstance(context);
-        mAd.setRewardedVideoAdListener(null);
+        mAd.setRewardedVideoAdListener(this);
         loadRewardedVideoAd();
     }
 
@@ -22,6 +25,48 @@ class AdProviderVideo {
     }
 
     void show() {
+        if (mAd.isLoaded()) {
+            mAd.show();
+        } else {
+            mClickedShow = true; // wait for onRewardedVideoAdLoaded()
+        }
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+        if (mClickedShow) {
+            mAd.show();
+        }
+        mClickedShow = false;
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+
+    }
+
+    @Override
+    public void onRewarded(RewardItem rewardItem) {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int i) {
 
     }
 }
