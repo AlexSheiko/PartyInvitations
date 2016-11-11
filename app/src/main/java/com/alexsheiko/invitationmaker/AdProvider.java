5a9f74f802eeafbdd7268a5547ceb1bb0a1a50c8
AlexeySheiko <alexeysheiko@hotmail.com>
2016-11-11 02:34:03 +0100

    Show image ad on tap of a template

--- app/src/main/java/com/alexsheiko/invitationmaker/AdProvider.java
+++ app/src/main/java/com/alexsheiko/invitationmaker/AdProvider.java
@@ -1,6 +1,7 @@
 package com.alexsheiko.invitationmaker;
 
 import android.content.Context;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
@@ -8,8 +9,10 @@ import com.google.android.gms.ads.InterstitialAd;
 public class AdProvider {
 
     private InterstitialAd mInterstitialAd;
+    private Context mContext;
 
     public void prepare(Context context) {
+        mContext = context;
         // Initialize the Mobile Ads SDK.
         mInterstitialAd = new InterstitialAd(context);
         mInterstitialAd.setAdUnitId("ca-app-pub-3038649646029056/5392277129");
@@ -26,7 +29,10 @@ public class AdProvider {
     }
 
     public void showImage() {
-        if (mInterstitialAd.isLoaded())
+        if (mInterstitialAd.isLoaded()) {
             mInterstitialAd.show();
+        } else {
+            Toast.makeText(mContext, "Retry in 2 seconds", Toast.LENGTH_SHORT).show();
+        }
     }
 }
