7c6c0fa287ded7bd57d2ecb15ef1d6ad2de57ac4
AlexeySheiko <alexeysheiko@hotmail.com>
2017-01-07 17:32:32 +0100

    Show free images first

--- app/src/main/java/com/alexsheiko/invitationmaker/MainActivity.kt
+++ app/src/main/java/com/alexsheiko/invitationmaker/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.elevation = 0f
     }
 
     fun onClickCategory(view: View) {
