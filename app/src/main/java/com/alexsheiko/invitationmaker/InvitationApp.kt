c6af79a78c1809e3bcf3b702321b395fbbc5eaaf
AlexeySheiko <alexeysheiko@hotmail.com>
2017-01-07 23:05:12 +0100

    Remove Adobe Creative SDK dependency

--- app/src/main/java/com/alexsheiko/invitationmaker/InvitationApp.kt
+++ app/src/main/java/com/alexsheiko/invitationmaker/InvitationApp.kt
@@ -1,41 +1,18 @@
 package com.alexsheiko.invitationmaker
 
-import android.support.multidex.MultiDexApplication
-
-import com.adobe.creativesdk.aviary.IAviaryClientCredentials
-import com.adobe.creativesdk.foundation.AdobeCSDKFoundation
-
+import android.app.Application
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig
 
 
-class InvitationApp : MultiDexApplication(), IAviaryClientCredentials {
+class InvitationApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        AdobeCSDKFoundation.initializeCSDKFoundation(applicationContext)
 
         // Init default font
         CalligraphyConfig.initDefault(CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/Brandon-Regular.otf")
                 .setFontAttrId(R.attr.fontPath)
                 .build())
-    }
-
-    override fun getClientID(): String {
-        return CREATIVE_SDK_CLIENT_ID
-    }
-
-    override fun getClientSecret(): String {
-        return CREATIVE_SDK_CLIENT_SECRET
-    }
-
-    override fun getBillingKey(): String {
-        return ""
-    }
-
-    companion object {
-
-        private val CREATIVE_SDK_CLIENT_ID = "56d4547128e84e079f6e1ef131607153"
-        private val CREATIVE_SDK_CLIENT_SECRET = "a46a3b39-4a9e-42c2-a5bc-6d7487e21943"
     }
 }
