4f72efd8aa7b8b7e2a56990dcf4be31bb92589e9
AlexeySheiko <alexeysheiko@hotmail.com>
2017-04-23 19:18:43 +0300

    ad banner

--- app/src/main/java/com/alexsheiko/invite/ui/MainActivity.kt
+++ app/src/main/java/com/alexsheiko/invite/ui/MainActivity.kt
@@ -3,17 +3,22 @@ package com.alexsheiko.invite.ui
 import android.graphics.Bitmap
 import android.graphics.Bitmap.CompressFormat.PNG
 import android.graphics.Color.WHITE
+import android.graphics.Typeface.BOLD
 import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings.Secure.ANDROID_ID
+import android.provider.Settings.Secure.getString
 import android.support.v4.content.FileProvider
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View.VISIBLE
 import android.widget.TextView.BufferType.EDITABLE
 import com.alexsheiko.invite.BuildConfig.DEBUG
 import com.alexsheiko.invite.R
 import com.alexsheiko.invite.util.prefs
 import com.alexsheiko.invite.util.shareImage
 import com.bumptech.glide.Glide
+import com.google.android.gms.ads.AdRequest.*
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.backgroundColor
 import org.jetbrains.anko.doAsync
@@ -21,9 +26,11 @@ import org.jetbrains.anko.onClick
 import org.jetbrains.anko.uiThread
 import java.io.File
 import java.io.FileOutputStream
-import java.util.*
+import java.util.Collections.shuffle
 
 class MainActivity : BaseActivity() {
+
+    var mShared = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +41,33 @@ class MainActivity : BaseActivity() {
 
         setClickListeners()
         reactToInput()
+        preloadAd()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (DEBUG || mShared) {
+            showAd()
+        }
+    }
+
+    private fun showAd() {
+        adView.visibility = VISIBLE
+    }
+
+    private fun preloadAd() {
+        val request = Builder()
+        request.setGender(GENDER_FEMALE)
+
+        if (DEBUG) {
+            val deviceId = getString(
+                    contentResolver,
+                    ANDROID_ID)
+            request.addTestDevice(deviceId)
+            request.addTestDevice(DEVICE_ID_EMULATOR)
+        }
+
+        adView.loadAd(request.build())
     }
 
     override fun onStop() {
@@ -47,22 +81,36 @@ class MainActivity : BaseActivity() {
     }
 
     private fun populateFields() {
+        val title = prefs.getString("title",
+                getString(R.string.title_default))
         val address = prefs.getString("address",
                 getString(R.string.details_default))
-        inputField.setText(address, EDITABLE)
+
+        inputTitle.setTypeface(null, BOLD)
+        inputTitle.setText(title, EDITABLE)
+        inputText.setText(address, EDITABLE)
+
+        titleView.text = title
         textView.text = address
     }
 
     private fun saveFields() {
-        val details = inputField.text.toString()
+        val title = inputTitle.text.toString()
+        val details = inputText.text.toString()
+
         if (!DEBUG && !details.startsWith("Enter")) {
             prefs.edit()
                     .putString("address", details)
                     .apply()
         }
+        prefs.edit()
+                .putString("title", title)
+                .apply()
     }
 
     private fun captureCanvas() {
+        mShared = true
+
         canvas.backgroundColor = WHITE
         canvas.isDrawingCacheEnabled = true
         canvas.buildDrawingCache()
@@ -94,7 +142,18 @@ class MainActivity : BaseActivity() {
     }
 
     private fun reactToInput() {
-        inputField.addTextChangedListener(object : TextWatcher {
+        inputTitle.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                titleView.text = s.toString()
+            }
+        })
+        inputText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
 
@@ -109,18 +168,19 @@ class MainActivity : BaseActivity() {
 
     private fun showImage() {
         val resId = R.drawable.party_template_5
-        Glide.with(this).load(resId).into(imageView)
+
+        Glide.with(this)
+                .load(resId)
+                .into(imageView)
     }
 
     private fun changeImage() {
-        val images = arrayOf(
+        val images = listOf(
                 R.drawable.party_template_1,
                 R.drawable.party_template_2,
                 R.drawable.party_template_4,
                 R.drawable.party_template_5,
-                R.drawable.party_template_6,
                 R.drawable.party_template_8,
-                R.drawable.wedding_template_1,
                 R.drawable.wedding_template_2,
                 R.drawable.wedding_template_3,
                 R.drawable.wedding_template_4,
@@ -128,8 +188,10 @@ class MainActivity : BaseActivity() {
                 R.drawable.wedding_template_7,
                 R.drawable.wedding_template_8)
 
-        val index = Random().nextInt(images.size - 1)
+        shuffle(images)
 
-        Glide.with(this).load(images[index]).into(imageView)
+        Glide.with(this)
+                .load(images[0])
+                .into(imageView)
     }
 }
