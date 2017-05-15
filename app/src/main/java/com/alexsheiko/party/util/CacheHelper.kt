37f1b38d413590d26c0563627f6975f186a64048
AlexeySheiko <alexeysheiko@hotmail.com>
2017-05-15 13:51:44 +0300

    change default template

--- app/src/main/java/com/alexsheiko/party/util/CacheHelper.kt
+++ app/src/main/java/com/alexsheiko/party/util/CacheHelper.kt
@@ -1,7 +1,7 @@
 package com.alexsheiko.party.util
 
-import android.graphics.Typeface
-import android.widget.TextView
+import android.graphics.Typeface.BOLD
+import android.widget.TextView.BufferType.EDITABLE
 import com.alexsheiko.party.R
 import com.alexsheiko.party.ui.MainActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,11 +12,11 @@ fun MainActivity.showTextAndImage() {
     val address = prefs.getString("address",
             getString(R.string.details_default))
     val image = prefs.getInt("image",
-            R.drawable.wedding_template_7)
+            R.drawable.party_template_5)
 
-    inputTitle.setTypeface(null, Typeface.BOLD)
-    inputTitle.setText(title, TextView.BufferType.EDITABLE)
-    inputText.setText(address, TextView.BufferType.EDITABLE)
+    inputTitle.setTypeface(null, BOLD)
+    inputTitle.setText(title, EDITABLE)
+    inputText.setText(address, EDITABLE)
 
     titleView.text = title
     textView.text = address
