eff858eaa8550090e1a855782c14890d928febb0
AlexeySheiko <alexeysheiko@hotmail.com>
2017-06-15 22:33:04 +0300

    remove calligraphy dependency

--- app/src/main/java/com/alexsheiko/party/App.kt
+++ app/src/main/java/com/alexsheiko/party/App.kt
@@ -1,12 +1,5 @@
 package com.alexsheiko.party
 
 import android.app.Application
-import com.alexsheiko.party.util.initCustomFont
 
-class App : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        initCustomFont()
-    }
-}
+class App : Application()
--- app/src/main/java/com/alexsheiko/party/ui/BaseActivity.kt
+++ app/src/main/java/com/alexsheiko/party/ui/BaseActivity.kt
@@ -1,17 +1,10 @@
 package com.alexsheiko.party.ui
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.FirebaseAnalytics.getInstance
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper.wrap
 
 abstract class BaseActivity : AppCompatActivity() {
 
     val mAnalytics: FirebaseAnalytics by lazy { getInstance(this) }
-
-    override fun attachBaseContext(newBase: Context) {
-        // Use custom font
-        super.attachBaseContext(wrap(newBase))
-    }
 }
